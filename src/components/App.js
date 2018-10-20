2e05785b0708fd10284eadfe752abd4481ecd73c
Jared <jarednutt@outlook.com>
2018-10-19 17:51:48 -0700

    removing routes file

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,18 @@
 import React, { Component } from 'react';
-import { isAuthenticated } from '../services/auth';
+import { Router } from '@reach/router';
+import Callback from './Callback';
 import Home from './pages/Home';
 import LandingPage from './pages/LandingPage';
 
+import { isAuthenticated } from '../services/auth';
+
 export default class App extends Component {
   render() {
-    return isAuthenticated() ? <Home /> : <LandingPage />;
+    return (
+      <Router>
+        {isAuthenticated() ? <Home path="/" /> : <LandingPage path="/" />}
+        <Callback path="callback" />
+      </Router>
+    );
   }
 }
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// class LambdaDemo extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { loading: false, msg: null };
-//   }
-
-//   handleClick = e => {
-//     e.preventDefault();
-
-//     this.setState({ loading: true });
-//     fetch('/.netlify/functions/hello')
-//       .then(response => response.json())
-//       .then(json => this.setState({ loading: false, msg: json.msg }));
-//   };
-
-//   render() {
-//     const { loading, msg } = this.state;
-
-//     return (
-//       <p>
-//         <button onClick={this.handleClick}>
-//           {loading ? 'Loading...' : 'Call Lambda'}
-//         </button>
-//         <br />
-//         <span>{msg}</span>
-//       </p>
-//     );
-//   }
-// }
